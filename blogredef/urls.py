d2dcc13ff4957ae27e9ea84d75b9f2b9bf44966d
kenju254 <kenmbugua@gmail.com>
2013-02-18 11:19:53 +0300

    This is the first commit to my project since start

---
+++ blogredef/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from blogengine.views import getPosts, getPost
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'blogredef.views.home', name='home'),
+    # url(r'^blogredef/', include('blogredef.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$' ,getPosts),
+    url(r'^(?P<selected_page>\d+)/?$', 'blogengine.views.getPosts'),
+    url(r'^(?P<postSlug>[-a-zA-Z0-9]+)/?$', getPost),
+)
