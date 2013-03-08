0f141c454275b7db1ad98732e629db00d078f875
kenju254 <kenmbugua@gmail.com>
2013-03-08 19:15:11 +0300

    changes to procfile and added deployment.py to settings module

---
+++ blogredef/settings/deployment.py
@@ -0,0 +1,21 @@
+import os
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'blog',                      # Or path to database file if using sqlite3.
+        'USER': 'blogpassword',                      # Not used with sqlite3.
+        'PASSWORD': 'blogpassword',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
